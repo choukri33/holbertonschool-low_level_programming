600218c13ad9da1058d62640c5eabab30216d6a3
choukri33 <7646@holbertonstudents.com>
2023-10-15 14:27:00 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- *  *main - prints exactly "Programming is like building a multilingual puzzle,
- *   *followed by a new line
- *    *
- *     *Return:  0 on success. Error code otherwise
- *      */
+
 
 int main(void)
 {
