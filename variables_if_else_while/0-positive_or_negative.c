7412e228708b1f3d82b82b79744ae7c33eb68bff
choukri33 <7646@holbertonstudents.com>
2023-10-16 12:57:30 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-  main - assign a random number to the variable n and
-  prints if n is positive,
-  equal to 0 or negative.
-  Return: Prints if n is positive, negative or equals to zero
- */
+/**
+ *  * main - assign a random number to the variable n and
+ *   * prints if n is positive,
+ *    * equal to 0 or negative.
+ *     * Return: Prints if n is positive, negative or equals to zero
+ *      */
 int main(void)
 {
 	int n;
