d3b7b5df8f33815bed41317756daeb597489f50f
choukri33 <7646@holbertonstudents.com>
2023-10-17 05:05:59 -0700

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <stdio.h>
 /**
- *  * main - prints the alphabet
- *   * Return: 0
- *    */
+*main - prints the alphabet
+*Return: 0
+*/
 int main(void)
 {
-	char alphabet = 'a';
+	char alphabet;
 
-	while (alphabet <= 'z')
-	{
-		putchar(alphabet);
-		alphabet++;
-	}
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{putchar(alphabet); }
 	putchar('\n');
 	return (0);
 }
