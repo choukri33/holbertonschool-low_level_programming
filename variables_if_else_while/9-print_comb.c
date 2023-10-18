ec042933cdc4460268a8fa9fe9bf56501aea566d
choukri33 <7646@holbertonstudents.com>
2023-10-18 03:49:29 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - prints all single digits from '0' to '9' with ","
+ *followed by a new line
+ *Return: 0
+ */
+int main(void)
+{
+	int digits = 0;
+
+	while (digits < 10)
+	{
+		putchar('0' + digits);
+		if (digits < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+
+		digits++;
+	}
+	return (0);
+}
