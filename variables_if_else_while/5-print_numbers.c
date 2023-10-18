5fce9d5e714eaba8c47bd968a02c30a9c4d2db8e
choukri33 <7646@holbertonstudents.com>
2023-10-18 00:40:37 -0700

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-
-int main(void){
-
-	int numbers = 0 ;
+/**
+ *main - prints all single digits from 0 to 9
+ *followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int numbers = 0;
 
 	while (numbers <= 9)
 	{
 		printf("%d", numbers);
 		numbers++;
-	}	
+	}
+
 	printf("\n");
 	return (0);
 }
