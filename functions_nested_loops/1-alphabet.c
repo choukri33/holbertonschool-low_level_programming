62ad38439027de76c0676a9f8248fc8aa5f924a7
choukri33 <7646@holbertonstudents.com>
2023-10-18 06:49:16 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int alphabet = 'a';
+	while(alphabet <='z')
+	{
+	_putchar (alphabet);
+	alphabet++;
+	}
+	_putchar('\n');
+	return (0);
+}
