afcaf004a569dc03432a5c043e2c31cbb627e053
choukri33 <7646@holbertonstudents.com>
2023-10-19 08:46:28 -0700

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *_abs - Entry point,computes the absolute variable of n
- *@a: character to compute
+ * _abs - Entry point,computes the absolute variable of n
+ * @n: character to compute
  *
- *Return: 0
+ * Return: 0
  */
+
 int _abs(int n)
 {
 	if (n < 0)
