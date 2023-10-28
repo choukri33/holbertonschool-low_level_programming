e9d4171e12c49d294b83d93b0fe29d17cc8b7137
choukri33 <7646@holbertonstudents.com>
2023-10-28 09:31:05 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle,
+ * followed by a new line
+ *
+ * Return:  0 on success. Error code otherwise
+ */
 
 int main(void)
 {
