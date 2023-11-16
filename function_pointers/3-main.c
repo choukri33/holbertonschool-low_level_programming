3802afa0e4962c6801e4360aa62ed18da96f2465
choukri33 <7646@holbertonstudents.com>
2023-11-16 08:35:44 -0800

    3-main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL)
+	{
+	printf("Error\n");
+	exit(99);
+	}
+	if (((*operator) == '/' || (*operator) == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
+}
