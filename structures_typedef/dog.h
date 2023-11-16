718d62588c9faf91d0c61e63ad172eefc6bdd500
choukri33 <7646@holbertonstudents.com>
2023-11-16 03:12:48 -0800

    1-init_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,19 @@
 #define DOG_H
 
 /**
- *  * struct dog - informations for a dog
- *   * @name: pointer to the name of the dog
- *    * @age: age of the dog
- *     * @owner: pointer to the owner of the dog
- *      *
- *       * Description: name of the dog, its age and its owner
- *        */
+ * struct dog - informations for a dog
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to the owner of the dog
+ *
+ * Description: name of the dog, its age and its owner
+ */
 
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
