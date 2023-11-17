093ab6181ade63766579822b9e9a7320120aee61
choukri33 <7646@holbertonstudents.com>
2023-11-17 07:15:16 -0800

    5-free_dog.c

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,5 +1,6 @@
 #define
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #ifndef
 #endif
