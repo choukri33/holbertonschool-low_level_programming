1e4f4ada4ded7dbf8f189540b8252157b62e2ea5
choukri33 <7646@holbertonstudents.com>
2023-11-27 10:56:57 -0800

    add_file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+	if(h->str == 0)
+	{
+	printf("[0] (nil)\n");
+	}
+	else
+	{
+	printf("[%u] [%s]\n",h->len, h->str);
+	}
+	h = h->next;
+	count++;
+	}
+	return count;
+}
