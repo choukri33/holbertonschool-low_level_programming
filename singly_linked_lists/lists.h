a721e6c634c489494cfdc97a7cb16a17b538588d
choukri33 <7646@holbertonstudents.com>
2023-11-30 00:34:46 -0800

    add_2-add_node.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 	typedef struct list_s
 	{
@@ -13,4 +14,6 @@
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
