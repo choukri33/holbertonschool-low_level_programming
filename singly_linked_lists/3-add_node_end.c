abd9f1e49e7515df9349a1e029e6464ad9251710
choukri33 <7646@holbertonstudents.com>
2023-11-30 03:51:03 -0800

    change_description

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,40 @@
 #include "lists.h"
 
 /**
-* add_node_end - adds a new node at the end of the list
-* @head: head of the list
-* @str: string to duplicate
-* Return: address of the new element or NULL if it fails
+* add_node_end - add a nex node aat the list
+* @head: head of list
+* @str: pointer
+* Return: address of the nex element
 */
+
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *ptr;
+	list_t *new_last, *tmp;
+	unsigned int len = 0;
 
-	new_node = malloc(sizeof(list_t));
-	ptr = *head;
-
-	if (new_node == NULL)
-	{
+	tmp = (*head);
+	new_last = malloc(sizeof(list_t));
+	if (new_last == NULL)
 		return (NULL);
+
+	for (; str[len]; len++)
+		;
+		new_last->str = strdup(str);
+		new_last->len = len;
+		new_last->next = (NULL);
+
+	if ((*head) == NULL)
+	{
+		*head = new_last;
+		return (new_last);
 	}
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	while (tmp->next != NULL)
 	{
-		free(new_node);
-		return (NULL);
+		tmp = tmp->next;
 	}
-	new_node->len = strlen(new_node->str);
-	new_node->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	while (ptr->next != NULL)
-	{
-		ptr = ptr->next;
-	}
-	ptr->next = new_node;
-	return (new_node);
+		tmp->next = new_last;
+
+	return (new_last);
 }
