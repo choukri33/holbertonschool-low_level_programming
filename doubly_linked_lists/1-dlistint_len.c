69166fd30bd443dbbb7cce015e8f8ae3e97d130f
choukri33 <7646@holbertonstudents.com>
2023-11-30 06:33:01 -0800

    add 1-dlistint_len

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in the list
+ * @h: pointer to the list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+		if (h->n != 0)
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
